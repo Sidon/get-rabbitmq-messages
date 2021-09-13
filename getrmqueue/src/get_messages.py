d19fc6f068fb1372fc034b9a1061f38c135fe31e
Sidon <sidoncd@gmail.com>
2021-09-13 20:26:48 -0300

    structuring for publish in PyPi

---
+++ getrmqueue/src/get_messages.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+import argparse, requests, csv, yaml, json, pprint
+
+
+__config_data = {}
+
+
+def __config_args(key_arg, __args, __arg_parser, __config_data):
+    if vars(__args)[key_arg] != __arg_parser.get_default(key_arg):
+        return vars(__args)[key_arg]
+    if key_arg not in __config_data:
+        return __arg_parser.get_default(key_arg)
+    return __config_data[key_arg]
+
+
+def config_parser(__arg_parser):
+    __arg_parser.add_argument(
+        '--configfile',
+        action='store',
+        help='Configuration file (full or relative path)',
+    )
+    __arg_parser.add_argument(
+        '--url',
+        action='store',
+        default='http://127.0.0.1:15672',
+        help='RabbitMQ server url without slash, default = http://127.0.0.1:15672.',
+    )
+    __arg_parser.add_argument(
+        '-v',
+        '--vhost',
+        action='store',
+        default='%2F',
+        help="RabbitMQ virtual server, default = '/'."
+    )
+    __arg_parser.add_argument(
+        '-q',
+        '--queue',
+        action='store',
+        default=None,
+        help='The queue from where the messages will be obtained.'
+    )
+    __arg_parser.add_argument(
+        '-c',
+        '--count',
+        action='store',
+        type=int,
+        default=10,
+        help='controls the maximum number of messages to get. Default=5.'
+    )
+    __arg_parser.add_argument(
+        '--user',
+        '-u',
+        action='store',
+        default='guest',
+        help='RabbitMQ user, default = guest.',
+    )
+    __arg_parser.add_argument(
+        '--password',
+        '-p',
+        action='store',
+        default='guest',
+        help='RabbitMQ password, default = guest.',
+    )
+    __arg_parser.add_argument(
+        '--outputfile',
+        '-o',
+        action='store',
+        default='./messages',
+        help='file for output messages.',
+    )
+    __arg_parser.add_argument(
+        '--separator',
+        '-s',
+        action='store',
+        default=None,
+        help='Character for line separator.',
+    )
+    __arg_parser.add_argument(
+        '--mode',
+        '-m',
+        action='store',
+        default='full',
+        help='full = whole message, payload = just payload',
+    )
+
+
+def build_data_args(__args, __arg_parser):
+    data_config = {}
+
+    if __args.configfile:
+        with open(__args.configfile, "r") as f:
+            data_config = yaml.safe_load(f)
+
+    for key in vars(__args):
+        data_config[key] = __config_args(key, __args, __arg_parser, data_config)
+
+    try:
+        data_config[
+            'url'] = f"{data_config['url']}/api/queues/{data_config['vhost']}/{data_config['queue']}/get"
+        data_config[
+            'payload_data'] = f"{{'count': {data_config['count']}, 'ackmode': 'ack_requeue_true', 'encoding': 'auto', 'truncate': 50000}}"
+        data_config['auth'] = (data_config['user'], data_config['password'])
+        data_config['headers'] = {'content-type': 'application/json', }
+    except Exception as error:
+        print("An exception occurred: ", error)
+        raise
+
+    return data_config
+
+
+def save_messages(data_args):
+    response = requests.post(data_args['url'], headers=data_args['headers'], data=data_args['payload_data'], auth=data_args['auth'])
+    separator = '\n'
+    if data_args['separator']:
+        separator = f"\n{80 * data_args['separator']}"
+
+    with open(data_args['outputfile'], 'w') as file_handler:
+        for msg in response.json():
+            message = msg if data_args['mode'] == 'full' else msg['payload']
+            output_message = f"\n{pprint.pformat(message, indent=4)}"
+            file_handler.write(output_message)
+            file_handler.write(separator)
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description='Get rabbitmq messages', allow_abbrev=False)
+    config_parser(arg_parser)
+    __args = arg_parser.parse_args()
+    data_args = build_data_args(__args, arg_parser)
+    save_messages(data_args)
+
